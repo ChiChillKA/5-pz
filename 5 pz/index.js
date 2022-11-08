1d77e67379f5815497d7a2f6f433af58ef0e2ce7
Roman Petreev <116781411+ChiChillKA@users.noreply.github.com>
2022-11-08 12:05:48 +0700

    Add files via upload

---
+++ 5 pz/index.js
@@ -0,0 +1,9 @@
+console.log("Задание 1")
+let a = []
+let m = 0
+let n = 100
+let count = 100
+for (let i = m; i < count; ++i ) {
+    a.push(Math.round(Math.random() * n))
+}
+console.log(a)
---
+++ 5 pz/index1.js
@@ -0,0 +1,7 @@
+console.log("Задание 2")
+var str = prompt("Введите слово: ")
+var strRev = ""
+for (i = str.length-1; i >= 0; i--) {
+  strRev += str[i]
+}
+console.log(strRev)
